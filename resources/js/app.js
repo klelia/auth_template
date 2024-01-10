d214322f5def465d8eb1e71a8481b0861a7a1cd7
klelia <clelia.fradella@gmail.com>
2024-01-10 16:59:56 +0100

    init

---
+++ resources/js/app.js
@@ -0,0 +1,10 @@
+import "./bootstrap";
+import "./bootstrap";
+import "~resources/scss/app.scss";
+import * as bootstrap from "bootstrap";
+import.meta.glob(["../img/**", "../fonts/**"]);
+// import Alpine from "alpinejs";
+
+// window.Alpine = Alpine;
+
+// Alpine.start();
